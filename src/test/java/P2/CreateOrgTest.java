d62c1464ee86347c312977a4f2ccddf989e69cf8
Ravikiran Jakkannavar <ravikiran3169@gmail.com>
2026-04-03 17:59:42 +0530

    commit

---
+++ src/test/java/P2/CreateOrgTest.java
@@ -0,0 +1,15 @@
+package P2;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+	public void createorgTest() {
+		System.out.println("executed orgTest");
+	}
+	@Test
+	public void modifyorgTest() {
+		System.out.println("executed modifyTest");
+	}
+
+}
---
+++ src/test/java/p1/CreateTest.java
@@ -0,0 +1,15 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class CreateTest {
+	@Test
+	public void createcontactTest() {
+		System.out.println("executed orgTest");
+	}
+	@Test
+	public void modifycontactTest() {
+		System.out.println("executed modifyTest");
+	}
+
+}
